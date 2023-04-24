2a70a6b5257082403e19c01eb8a37e5563368812
Devun Durst <devunfoxdurst@yahoo.com>
2023-04-24 12:07:01 -0600

    C#.3Pra

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -7,9 +7,39 @@ class Program //Describes what this class is
     static void Main(string[] args)
     //the entry point into the program
     {
-        Random randomGenerator = new Random();
-        int number = randomGenerator.Next(1,11);
-        Console.WriteLine(number);
+        string Play = "";
+        while (Play != "N")
+        {
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1,100);
+            Console.WriteLine($"What is the magic number? {number}");
+            int userGuess = 0;
+            int count = 0;
+            while (number != userGuess)
+            {
+                Console.Write("What is your guess? ");
+                string answer = Console.ReadLine();
+                userGuess = int.Parse(answer);
+                count += 1;
+
+                if (number > userGuess) 
+                {
+                        Console.WriteLine("higher");
+                }
+                else if (number < userGuess)
+                {
+                    Console.WriteLine("lower");
+                }
+                else if (number == userGuess)
+                {
+                    Console.WriteLine($"You guessed it! It took you {count} tries.");
+                }
+                
+            }
+            Console.WriteLine(" ");
+            Console.WriteLine("Would you like to play again? (Y/N)");
+            Play = Console.ReadLine();
+        }
     }
 }
 //end of the program
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,12 @@
 using System;
 
+namespace Learning02 // call it by project name
+{
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
     }
+}
 }
