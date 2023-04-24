19344d207a5dd747df95c469851a182517b681e5
Devun Durst <devunfoxdurst@yahoo.com>
2023-04-24 09:22:59 -0600

    update

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,10 +1,15 @@
 using System;
 
-class Program
+//the entry point of our program
+class Program //Describes what this class is
+// 
 {
     static void Main(string[] args)
+    //the entry point into the program
     {
-
-        Console.WriteLine("");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,11);
+        Console.WriteLine(number);
     }
 }
+//end of the program
