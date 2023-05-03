29adf64fc9ff34a7d0d5313117e04150e67c5717
Devun Durst <devunfoxdurst@yahoo.com>
2023-05-02 18:02:59 -0600

    new

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,12 +1,26 @@
+// 1) Class name
+// 2) Responsibilty
+// 3) variables are now attributes
+// 4) function are now methods
 using System;
 
-namespace Learning02 // call it by project name
+public class Resume
 {
-class Program
-{
-    static void Main(string[] args)
+    public string _name;
+
+    // Make sure to initialize your list to a new List before you use it.
+    public List<Job> _jobs = new List<Job>();
+
+    public void Display()
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+
+        // Notice the use of the custom data type "Job" in this loop
+        foreach (Job job in _jobs)
+        {
+            // This calls the Display method on each job
+            job.Display();
+        }
     }
-}
 }
