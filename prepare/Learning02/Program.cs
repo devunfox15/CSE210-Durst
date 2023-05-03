28634db25beb388331591ee8fbd76c7bdfa86645
Devun Durst <devunfoxdurst@yahoo.com>
2023-05-03 09:03:45 -0600

    nameSpace

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,27 +1,28 @@
 using System;
-
-class Program
-{
-    static void Main(string[] args)
+namespace Learning02{
+    class Program
     {
-        Job job1 = new Job();
-        job1._jobTitle = "Delivery Driver";
-        job1._company = "Domino's Pizza";
-        job1._startYear = 2019;
-        job1._endYear = 2022;
+        static void Main(string[] args)
+        {
+            Job job1 = new Job();
+            job1._jobTitle = "Delivery Driver";
+            job1._company = "Domino's Pizza";
+            job1._startYear = 2019;
+            job1._endYear = 2022;
 
-        Job job2 = new Job();
-        job2._jobTitle = "Delivery Driver";
-        job2._company = "Papa John's pizza";
-        job2._startYear = 2022;
-        job2._endYear = 2023;
+            Job job2 = new Job();
+            job2._jobTitle = "Delivery Driver";
+            job2._company = "Papa John's pizza";
+            job2._startYear = 2022;
+            job2._endYear = 2023;
 
-        Resume myResume = new Resume();
-        myResume._name = "Devun Fox Durst";
+            Resume myResume = new Resume();
+            myResume._name = "Devun Fox Durst";
 
-        myResume._jobs.Add(job1);
-        myResume._jobs.Add(job2);
+            myResume._jobs.Add(job1);
+            myResume._jobs.Add(job2);
 
-        myResume.Display();
+            myResume.Display();
+        }
     }
 }
