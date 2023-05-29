7d87d9b98d11b4bcf8d76f23837ddcce41f3f9a8
Devun Durst <devunfoxdurst@yahoo.com>
2023-05-29 12:23:33 -0600

    learning activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment student1 = new Assignment("Samuel Bennett", "Multiplication");
+        string StudentName = student1.GetStudentName();
+        string topic = student1.GetTopic();
+        Console.WriteLine(StudentName);
+        Console.WriteLine(topic);
+        Console.WriteLine();
+
+        Math mathwork = new Math("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathwork.GetStudentTopic());
+        Console.WriteLine(mathwork.GetHomeworkList());
+
+        Console.WriteLine();
+
+        Writing writingWork = new Writing("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writingWork.GetStudentTopic());
+        Console.WriteLine(writingWork.GetWritingInformation());
     }
 }
